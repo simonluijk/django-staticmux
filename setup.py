aa83372ff16e5da2e7a9429471072b8bd19a8d36
Simon Luijk <simon@luijk.co.uk>
2011-07-18 18:11:11 +0200

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name = 'staticmux',
+    description = 'Django app to simplifiy development and deployment of CSS and Javascript for Django projects.',
+    version = '0.0.1',
+    author = 'Simon Luijk',
+    author_email = 'simon@luijk.co.uk',
+    url = 'http://www.apricotwebsolutions.com/',
+    packages = [
+        'staticmux',
+        'staticmux.conf',
+        'staticmux.filters',
+        'staticmux.management',
+        'staticmux.management.commands',
+        'staticmux.templatetags',
+    ],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
